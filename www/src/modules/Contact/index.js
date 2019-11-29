7696175aeaa5b0b030e25757f9d86da4bce7fdc8
= <vitor.hidalgo@gmail.com>
2019-11-29 17:32:13 -0300

    teste

--- www/src/modules/Contact/index.js
+++ www/src/modules/Contact/index.js
@@ -1,53 +1,63 @@
-import React from 'react';
-import * as Yup from 'yup';
+import React from "react";
+import * as Yup from "yup";
 
-import {
-    Container,
-    FForm,
-    Label,
-    LabelFull,
-    FInput
-} from './styles';
-import { Button } from '../../styles/global';
+import { Container, FForm, Label, LabelFull, FInput } from "./styles";
+import { Button } from "../../styles/global";
 
 const schema = Yup.object().shape({
-    name: Yup.string()
-        .required('Campo Obrigatório'),
-    email: Yup.string()
-        .email('E-mail inválido')
-        .required('Campo Obrigatório'),
-    message: Yup.string()
-        .required('Mensagem Obrigatória')
+  name: Yup.string().required("Campo Obrigatório"),
+  email: Yup.string()
+    .email("E-mail inválido")
+    .required("Campo Obrigatório"),
+  message: Yup.string().required("Mensagem Obrigatória")
 });
 
 export default function Contact() {
-    function submitHandler(data, { resetForm }) {
-        console.log(data);
-        resetForm();
-    }
+  function submitHandler(data, { resetForm }) {
+    console.log(data);
+    resetForm();
+  }
 
-    return(
-        <Container>
-            <div className="content">
-                <h2>Contato</h2>
-                <FForm schema={schema} onSubmit={submitHandler}>
-                    <Label htmlFor={'name'}>
-                        <FInput name={'name'} id={'iptName'} placeholder={'Digite seu nome'} />
-                    </Label>
-                    <Label htmlFor={'email'}>
-                        <FInput name={'email'} id={'iptEmail'} placeholder={'Digite seu e-mail'} />
-                    </Label>
-                    <Label htmlFor={'phone'}>
-                        <FInput name={'phone'} id={'iptPhone'} placeholder={'Digite seu telefone'} />
-                    </Label>
-                    <LabelFull htmlFor={'message'}>
-                        <FInput multiline className="textarea" name={'message'} id={'txtMessage'} placeholder={'Digite sua mensagem'} />
-                    </LabelFull>
-                    <Label>
-                        <Button>ENVIAR MENSAGEM</Button>
-                    </Label>
-                </FForm>
-            </div>
-        </Container>
-    );
+  return (
+    <Container>
+      <div className="content">
+        <h2>Contato</h2>
+        <FForm schema={schema} onSubmit={submitHandler}>
+          <Label htmlFor={"name"}>
+            <FInput
+              name={"name"}
+              id={"iptName"}
+              placeholder={"Digite seu nome"}
+            />
+          </Label>
+          <Label htmlFor={"email"}>
+            <FInput
+              name={"email"}
+              id={"iptEmail"}
+              placeholder={"Digite seu e-mail"}
+            />
+          </Label>
+          <Label htmlFor={"phone"}>
+            <FInput
+              name={"phone"}
+              id={"iptPhone"}
+              placeholder={"Digite seu telefone"}
+            />
+          </Label>
+          <LabelFull htmlFor={"message"}>
+            <FInput
+              multiline
+              className="textarea"
+              name={"message"}
+              id={"txtMessage"}
+              placeholder={"Digite sua mensagem"}
+            />
+          </LabelFull>
+          <Label>
+            <Button>ENVIAR MENSAGEM</Button>
+          </Label>
+        </FForm>
+      </div>
+    </Container>
+  );
 }
